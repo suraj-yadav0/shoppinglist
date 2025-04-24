2e8368d9352c51f56151140d64a57ac601d213dc
Suraj Yadav <surajyadav200701@gmail.com>
2025-04-24 15:13:16 +0530

    Connected Flask Sqlite Together where we have defined a pricelist

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 from random import randrange
+from sqlite3 import connect
 
-from flask import Flask, request
+from flask import Flask, g, request
 
 app = Flask(__name__)
+DATABASE = "itemprices.sqlite"
+
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 
 @app.route("/")
@@ -12,4 +35,11 @@ def hello_world():
 
 @app.route("/itemprice")
 def itemprice():
-    return {"name": request.args.get("itemname"), "price": randrange(1000) / 100}
+    itemname = request.args.get("itemname")
+    try:
+        price = query_db(
+            "SELECT price from Pricelist where name = ?", [itemname], one=True
+        )[0]
+    except TypeError:
+        price = 0
+    return {"name": itemname, "price": price}
